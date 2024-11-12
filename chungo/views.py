37f075a87f07c86781d82cdbfc3d4fab32698750
Tomimarconi92 <tomimarconi92@hotmail.com>
2024-11-12 15:57:48 -0300

    1.02

--- chungo/views.py
+++ chungo/views.py
@@ -5,7 +5,15 @@ def helados_view(request):
     return render(request, 'helados.html')
 
 
-
 def helados_view(request):
-    helados = GustoHelado.objects.filter().all()
-    return render(request, 'helados.html',{'gustos' : helados})
+    cremas = GustoHelado.objects.filter(categoria='CREMAS').all()
+    cremas_frutales = GustoHelado.objects.filter(categoria='CREMAS FRUTALES').all()
+    chocolate = GustoHelado.objects.filter(categoria='CHOCOLATE').all() 
+    dulce_de_leche = GustoHelado.objects.filter(categoria='DULCE DE LECHE').all()
+    frutales = GustoHelado.objects.filter(categoria='FRUTALES').all()
+    return render(request, 'helados.html',{'cremas' : cremas,
+                                           'cremas_frutales' : cremas_frutales,
+                                           'chocolate' : chocolate,
+                                           'dulce_de_leche' : dulce_de_leche,
+                                           'frutales' : frutales
+                                           })
--- core/models.py
+++ core/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 class GustoHelado(models.Model):
-    CATEGORIAS = [('crema', 'Crema'), 
-                  ('frutal', 'Frutal')
-                  ]
+    CATEGORIAS = [('CREMAS','CREMAS'),
+                  ('CREMAS FRUTALES','CREMAS FRUTALES'),
+                  ('CHOCOLATE','CHOCOLATE'),
+                  ('DULCE DE LECHE','DULCE DE LECHE'),
+                  ('FRUTALES','FRUTALES')
+                ]
 
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
